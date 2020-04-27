acbef2699ed0f75125fef56019299556ddd0b9b2
Omar Elhayes <oelhay2@illinois.edu>
2020-04-26 23:22:25 -0500

    implement User class and part of Book class

--- app/src/main/java/Book.java
+++ app/src/main/java/Book.java
@@ -1,13 +1,25 @@
+import java.util.List;
+
 public class Book {
     int pages;
     String genre;
     String author;
     String title;
-    Book (int setPages, String setGenere, String setAuthor, String setTitle) {
+    List<Integer> pageNumbers;
+    Book (int setPages, String setGenre, String setAuthor, String setTitle) {
         pages = setPages;
-        genre = setGenere;
+        genre = setGenre;
         author = setAuthor;
         title = setTitle;
+        for (int i = 0; i < pages; i++) {
+            pageNumbers.add(i + 1);
+        }
     }
-
+    public void bookMark(int currentPage) {
+        for (int i = 0; i < pageNumbers.size(); i++) {
+            if (pageNumbers.get(i) == currentPage) {
+                pageNumbers.add(i, 0);
+            }
+        }
+    }
 }
--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,16 +1,41 @@
 import java.util.List;
 
 public class User {
+    /**
+     * list of books user has read.
+     */
     List<String> books;
+    /**
+     * list of the genres of the books the user has read.
+     */
     List<String> genres;
+    /**
+     * list of the authors of the books the user has read.
+     */
     List<String> authors;
-    List<Integer> pageNum
+    /**
+     * list of the page numbers of the books the user has read.
+     */
+    List<Integer> pageNum;
+
+    /**
+     * sets all the lists above
+     * @param setBooks books read
+     * @param setGenres genres read
+     * @param setAuthors authors of books read
+     * @param setPageNum page numbers of books read.
+     */
     User (List<String> setBooks, List<String> setGenres, List<String> setAuthors, List<Integer> setPageNum) {
         books = setBooks;
         genres = setGenres;
         authors = setAuthors;
         pageNum = setPageNum;
     }
+
+    /**
+     * calculates the total number of pages the user has read.
+     * @return the total number of pages read by the user.
+     */
     public int totalPages() {
         int toReturn = 0;
         for (int i = 0; i < pageNum.size(); i++) {
@@ -18,26 +43,64 @@ public class User {
         }
         return toReturn;
     }
+
+    /**
+     * gives the user's favorite author out of all the books they have read.
+     * @return the user's favorite author.
+     */
     public String favAuthor() {
         List<Integer> num = pageNum;
         for (int i = 0; i < authors.size(); i++) {
             int count = 0;
             for (int j = 0; j < authors.size(); i++) {
-                if (i == j) {
+                if (authors.get(i) == authors.get(j)) {
                     count++;
                 }
             }
             num.add(count, i);
         }
         int max = 0;
+        int index = 0;
         for (int i = 0; i < num.size(); i++) {
-            if (max < ) {
-
+            if (max < num.get(i)) {
+                max = num.get(i);
+                index = i;
             }
         }
-    }
-    public String favGenre(genres) {
-
+        return authors.get(index);
     }
 
+    /**
+     * gives the user's favorite genre out of all the books they have read.
+     * @return returns the user's favorite genre.
+     */
+    public String favGenre() {
+        List<Integer> num = pageNum;
+        for (int i = 0; i < genres.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < genres.size(); i++) {
+                if (genres.get(i) == genres.get(j)) {
+                    count++;
+                }
+            }
+            num.add(count, i);
+        }
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < num.size(); i++) {
+            if (max < num.get(i)) {
+                max = num.get(i);
+                index = i;
+            }
+        }
+        return genres.get(index);
+    }
+
+    /**
+     * the current book the user is reading or last book the user read.
+     * @return the current book the user is reading
+     */
+    public String currentBook() {
+        return books.get(books.size() - 1);
+    }
 }
