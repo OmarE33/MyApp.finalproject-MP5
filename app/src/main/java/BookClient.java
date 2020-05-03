4ef610ca630c94ec5268bb318f4ac318f4956542
Omar Elhayes <oelhay2@illinois.edu>
2020-05-02 21:30:16 -0500

    implement User class and part of Book class some UI Internet access and Volley implementation Tried the GET request

--- app/src/main/java/BookClient.java
+++ app/src/main/java/BookClient.java
@@ -1,6 +1,28 @@
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class BookClient {
-    BookClient() {
-
+    String resourceID;
+    String url = "https://www.googleapis.com/books/v1/volumes/" + resourceID;
+    JSONObject toUse;
+    BookClient(String volumeID) {
+        resourceID = volumeID;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        toUse = response;
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
     }
-
 }
