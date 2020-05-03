a1f3d1c066bf4595f9e7a537b5638119e982c681
Omar Elhayes <oelhay2@illinois.edu>
2020-05-03 13:33:18 -0500

    implement User class and part of Book class some UI Internet access and Volley implementation Tried the GET request search request

--- app/src/main/java/BookClient.java
+++ app/src/main/java/BookClient.java
@@ -1,3 +1,5 @@
+import android.widget.Button;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -8,6 +10,7 @@ import org.json.JSONObject;
 
 public class BookClient {
     String resourceID;
+    private static final String KEY_ = "AIzaSyDPRMo_AtgJgG-Tn0cDej3_Lwiarrc2LZc";
     String url = "https://www.googleapis.com/books/v1/volumes/" + resourceID;
     JSONObject toUse;
     BookClient(String volumeID) {
@@ -17,6 +20,22 @@ public class BookClient {
                     @Override
                     public void onResponse(JSONObject response) {
                         toUse = response;
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+    }
+
+    private void search() {
+        String searchURL = "https://www.googleapis.com/books/v1/volumes?q=" + "searchedterm" + "+intitle";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, searchURL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // sets the list to books matching search word in title
                     }
                 }, new Response.ErrorListener() {
                     @Override
