3e33895422f469941852cb63774c76d8fbea284c
Eric An <jan31@illinois.edu>
2020-05-06 04:11:24 +0900

    working on User profile

--- app/src/main/java/com/example/myapp/Userprofile.java
+++ app/src/main/java/com/example/myapp/Userprofile.java
@@ -3,12 +3,26 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Userprofile extends AppCompatActivity {
+    Button run;
+    TextView favgenre;
+    TextView favauthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
+        favgenre = (TextView)findViewById(R.id.favgenre);
+        run = (Button)findViewById(R.id.calculateButton);
+        run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                favgenre.setText();
+            }
+        });
     }
 }
