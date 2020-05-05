ed00fa41d95f9f6c244675200a980c7488c1f8cb
Omar Elhayes <oelhay2@illinois.edu>
2020-05-04 19:44:39 -0500

    implement User class and part of Book class some UI Internet access and Volley implementation Tried the GET request search request

--- app/src/main/java/com/example/myapp/BookClient.java
+++ app/src/main/java/com/example/myapp/BookClient.java
@@ -1,9 +1,18 @@
 package com.example.myapp;
 
+import android.content.Context;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,6 +30,7 @@ public class BookClient {
     public BookClient() {
 
     }
+
     public Book searchSpecific(String volumeLink) {
         url = volumeLink + "?" + KEY_;
         final Book first = new Book();
@@ -29,6 +39,7 @@ public class BookClient {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            System.out.println("here");
                             cover = (String) response.get("thumbnail");
                             int pages = (int) response.get("pageCount");
                             String title = (String) response.get("title");
@@ -59,6 +70,7 @@ public class BookClient {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            System.out.println("here");
                             BookClient search = new BookClient();
                             JSONArray array = response.getJSONArray("items");
                             for (int i = 0; i < array.length(); i++) {
--- app/src/main/java/com/example/myapp/Recentbooks.java
+++ app/src/main/java/com/example/myapp/Recentbooks.java
@@ -2,46 +2,43 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.sip.SipSession;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.R.layout;
 import android.widget.SearchView;
 
 public class Recentbooks extends AppCompatActivity {
     private BookListAdapter adapter;
+    List<Book> listToAdd = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recentbooks);
-        ListView list = findViewById(R.id.theList);
-        SearchView searchBar = findViewById(R.id.search_bar);
-        BookClient searchBooks = new BookClient();
-        List<Book> listToAdd = new ArrayList<>();
-
-
-        //listToAdd = searchBooks.search(searchBar.getQuery().toString());
-        Book harry = new Book(120, "omar", "harry Potter");
-        listToAdd.add(harry);
+        final ListView list = findViewById(R.id.theList);
+        final SearchView searchBar = findViewById(R.id.search_bar);
+        final BookClient searchBooks = new BookClient();
 
         adapter = new BookListAdapter(this, R.layout.activity_custom, listToAdd);
-        list.setAdapter(adapter);
+        System.out.println("oncreate");
 
         searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 adapter.getFilter().filter(query);
+                listToAdd = searchBooks.search(searchBar.getQuery().toString());
+
+                list.setAdapter(adapter);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                listToAdd = searchBooks.search(searchBar.getQuery().toString());
+
+                list.setAdapter(adapter);
                 return false;
             }
         });
